4cf1c8f58e06a76e2b8fee72709b45c111b54ace
songyang <610467559@qq.com>
2022-07-19 17:20:35 +0800

    添加坐标 光源  阴影

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,21 +10,70 @@ onMounted(() => {
   const { innerWidth, innerHeight } = window
   const renderer = new THREE.WebGLRenderer()
   renderer.setSize(innerWidth, innerHeight)
+  renderer.shadowMap.enabled = true
   const container = document.querySelector('#p-index')
   container?.appendChild(renderer.domElement)
 
+  // 创建场景
   const scene = new THREE.Scene()
+  // 创建相机
   const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
-  const geometry = new THREE.BoxGeometry()
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ffee })
 
-  const mesh = new THREE.Mesh(geometry, material)
+  // 创建一个几何体
+  const geometry = new THREE.BoxGeometry(8, 8, 8)
+  // 创建几何体的表面材质
+  const material = new THREE.MeshLambertMaterial({ color: 0xff2288 })
 
-  scene.add(mesh)
+  // 生成网格
+  const cube = new THREE.Mesh(geometry, material)
 
-  camera.position.z = 5
-  mesh.rotation.x = 0.5
-  mesh.rotation.y = 0.5
+  // 设置一个几何体网格的属性
+  cube.castShadow = true // 显示阴影
+  // 位置设置
+  cube.position.x = 4
+  cube.position.y = 10
+  cube.position.z = 20
+
+  scene.add(cube)
+
+  // 创建坐标轴
+  const axes = new THREE.AxesHelper(100)
+  scene.add(axes)
+
+  // 创建地板
+  const planeGeometry = new THREE.PlaneGeometry(80, 80) // 几何体
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc }) // 材质
+  const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+
+  // 位置信息
+  plane.rotation.x = -0.5 * Math.PI
+  plane.position.set(-15, 0, 0)
+  plane.receiveShadow = true
+
+  scene.add(plane)
+
+  // 光源
+  const spotLight = new THREE.SpotLight(0xffffff)
+
+  spotLight.position.set(-60, 40, -65)
+  spotLight.castShadow = true
+  spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024)
+  spotLight.shadow.camera.far = 130
+  spotLight.shadow.camera.near = 40
+
+  scene.add(spotLight)
+
+  const ambientLight = new THREE.AmbientLight(0xaaaaaa)
+
+  scene.add(ambientLight)
+
+  // cube.rotation.x += 0.5
+  // cube.rotation.y += 0.5
+
+  camera.position.x = -30
+  camera.position.y = 45
+  camera.position.z = 35
+  camera.lookAt(scene.position)
 
   renderer.render(scene, camera)
 })
