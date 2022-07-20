26c814a85e7991291daff343ddf2182474609777
songyang <610467559@qq.com>
2022-07-20 15:54:38 +0800

    feat: part7

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -38,8 +38,15 @@ onMounted(() => {
 
   // 创建场景
   const scene = new THREE.Scene()
+  // scene.fog = new THREE.Fog(0x0000ff, 0.01, 110)
+  scene.fog = new THREE.FogExp2(0x0000ff, 0.01)
   // 创建相机
   const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000)
+
+  // 创建坐标轴
+  const axes = new THREE.AxesHelper(100)
+  axes.setColors(new THREE.Color(0xff00ff), new THREE.Color(0xffff00), new THREE.Color(0x00ffff))
+  scene.add(axes)
 
   // 创建一个几何体
   const geometry = new THREE.BoxGeometry(8, 8, 8)
@@ -61,13 +68,19 @@ onMounted(() => {
 
   scene.add(cube)
 
-  // 创建坐标轴
-  const axes = new THREE.AxesHelper(100)
-  axes.setColors(new THREE.Color(0xff00ff), new THREE.Color(0xffff00), new THREE.Color(0x00ffff))
-  scene.add(axes)
+  // 常见一个新的几何体
+
+  const geometry2 = new THREE.BoxGeometry(4, 4, 4)
+  const material2 = new THREE.MeshLambertMaterial({ color: 0x00ffee })
+  const cube2 = new THREE.Mesh(geometry2, material2)
+
+  cube2.position.set(-10, 4, 0)
+  cube2.name = 'cube2'
+
+  scene.add(cube2)
 
   // 创建地板
-  const planeGeometry = new THREE.PlaneGeometry(250, 250) // 几何体
+  const planeGeometry = new THREE.PlaneGeometry(125, 125) // 几何体
   const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc }) // 材质
   const plane = new THREE.Mesh(planeGeometry, planeMaterial)
 
@@ -85,7 +98,7 @@ onMounted(() => {
   spotLight.castShadow = true
   spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024)
   spotLight.shadow.camera.far = 130
-  spotLight.shadow.camera.near = 40
+  spotLight.shadow.camera.near = 50
 
   scene.add(spotLight)
 
@@ -93,28 +106,61 @@ onMounted(() => {
 
   scene.add(ambientLight)
   // dat.gui 输入控制变量的名称
-  const ctrlObj = { rotation: 0.01, jump: 0.01 }
+  const cube2Obj = scene.getObjectByName('cube2')
+
+  const ctrlObj = {
+    rotation: 0.01,
+    jump: 0.01,
+    removeCube2: () => {
+      if (cube2Obj instanceof THREE.Mesh) {
+        scene.remove(cube2Obj)
+      }
+    },
+    addNewCube() {
+      const geometryN = new THREE.BoxGeometry(5, 5, 5)
+      const materialN = new THREE.MeshLambertMaterial({ color: 0x00ef0f })
+      const cubeN = new THREE.Mesh(geometryN, materialN)
+      cubeN.position.set(-Math.random() * 10, Math.random() * 10, Math.random() * 10)
+      cubeN.rotation.set(Math.random(), Math.random(), Math.random())
+      scene.add(cubeN)
+    }
+  }
 
   const ctrl = new dat.GUI()
   ctrl.add(ctrlObj, 'rotation', 0, 1)
   ctrl.add(ctrlObj, 'jump', 0, 1)
+  ctrl.add(ctrlObj, 'removeCube2')
+  ctrl.add(ctrlObj, 'addNewCube')
   ctrl.domElement.style.zIndex = '10'
 
   camera.position.x = -30
-  camera.position.y = 60
-  camera.position.z = 100
+  camera.position.y = 50
+  camera.position.z = 50
   camera.lookAt(scene.position)
+  // camera.lookAt(new THREE.Vector3(0, 0, 0))
 
   let gap = 0
 
-  function renderScene() {
-    cube.rotation.x += ctrlObj.rotation
-    cube.rotation.y += ctrlObj.rotation
-    cube.rotation.z += ctrlObj.rotation
+  // cube.rotation.x += 0.75
+  // cube.rotation.y += 0.75
+  // cube.rotation.z += 0.75
 
-    gap += ctrlObj.jump
-    cube.position.x = 25 + 20 * Math.sin(gap)
-    cube.position.y = 6 + 20 * Math.abs(Math.cos(gap))
+  function renderScene() {
+    // cube.rotation.x += ctrlObj.rotation
+    // cube.rotation.y += ctrlObj.rotation
+    // cube.rotation.z += ctrlObj.rotation
+
+    // gap += ctrlObj.jump
+    // cube.position.x = 25 + 20 * Math.sin(gap)
+    // cube.position.y = 6 + 20 * Math.abs(Math.cos(gap))
+
+    scene.traverse(obj => {
+      if (obj instanceof THREE.Mesh && obj !== plane) {
+        obj.rotation.x += ctrlObj.rotation
+        obj.rotation.y += ctrlObj.rotation
+        obj.rotation.z += ctrlObj.rotation
+      }
+    })
 
     requestAnimationFrame(renderScene)
     renderer.render(scene, camera)
